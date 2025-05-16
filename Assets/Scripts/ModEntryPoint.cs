3f8e611e9ea93aa4b5be311856ee46bce1be4f3a
d_martynenko <d_martynenko@atomrpg.com>
2025-05-16 11:59:36 +0300

    mod build fix

--- Assets/Scripts/ModEntryPoint.cs
+++ Assets/Scripts/ModEntryPoint.cs
@@ -1,5 +1,4 @@
-﻿//#define SUPPORT_LEVEL_BUNDLE // Managed via build mod tool.
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using JSon;
@@ -29,74 +28,18 @@ public class ModEntryPoint : MonoBehaviour // ModEntryPoint - RESERVED LOOKUP NA
 
     void LoadModBundle()
     {
+        ResourceManager.DEBUG_MODE = true;
 #if UNITY_EDITOR
         // skip bundle loading in PIE mode
 #else
         ResourceManager.AddBundle(modName + "_resources", AssetBundle.LoadFromFile(dir + "/" + modName + "_resources"));
-        ResourceManager.AddBundle(modName + "_scenes", AssetBundle.LoadFromFile(dir + "/" + modName + "_scenes"));
-#if SUPPORT_LEVEL_BUNDLE
-        AssetBundle assetBundle = AssetBundle.LoadFromFile(dir + "/" + modName);
-        if (assetBundle != null)
-        {
-            manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
-        }
-        GlobalEvents.AddListener<GlobalEvents.PrepareNextLevel>(PrepareNextLevel);
-#endif
+        ResourceManager.AddSceneBundle(modName + "_scenes", AssetBundle.LoadFromFile(dir + "/" + modName + "_scenes"));
 #endif
     }
-
-#if SUPPORT_LEVEL_BUNDLE
-    AssetBundleManifest manifest;
-    List<AssetBundle> lastLevelBundle = new List<AssetBundle>();
-    void PrepareNextLevel(GlobalEvents.PrepareNextLevel evnt)
-    {
-        if (manifest != null)
-        {
-            if (lastLevelBundle.Count > 0)
-            {
-                Debug.Log("Unload last level bundle: " + evnt.levelName);
-
-                foreach (var bundle in lastLevelBundle)
-                {
-                    ResourceManager.RemoveBundle(bundle, true);
-                }
-            }
-
-
-            AssetBundle b;
-
-            foreach (var bundle in manifest.GetAllDependencies(modName + "_" + evnt.levelName))
-            {
-                if(bundle.Contains("_resources"))
-                {
-                    continue; // skip default resources pack
-                }
-
-                Debug.Log("Load shared level bundle: " + bundle);
-
-                b = AssetBundle.LoadFromFile(dir + "/" + bundle);
-                if (b != null)
-                {
-                    ResourceManager.AddBundle(bundle, b);
-                    lastLevelBundle.Add(b);
-                }
-            }
-
-            Debug.Log("Load level bundle: " + evnt.levelName);
-            b = AssetBundle.LoadFromFile(dir + "/" + modName + "_" + evnt.levelName);
-            if (b != null)
-            {
-                ResourceManager.AddBundle(modName, b);
-                lastLevelBundle.Add(b);
-            }
-        }
-    }
-#endif
 
     void GameLoaded(GlobalEvents.GameStart evnt)
     {
         Localization.LoadStrings("mymod_strings_", Localization.Language);
-        //Game.World.console.DeveloperMode();
     }
 
     void LevelLoaded(GlobalEvents.LevelLoaded evnt)
